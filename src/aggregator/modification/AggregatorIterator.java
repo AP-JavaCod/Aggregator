2e1370ae2ed8dba04cd44048c1ded2b41996d8db
AP-JavaCod <megafoh3g@gmail.com>
2024-08-20 21:14:16 +0500

    Creation AggregatorIterator.java

---
+++ src/aggregator/modification/AggregatorIterator.java
@@ -0,0 +1,56 @@
+package aggregator.modification;
+
+import aggregator.lambda.CalculationModification;
+
+import java.util.Iterator;
+
+public class AggregatorIterator <T, U> implements Iterable<U>{
+
+    private final String NAME;
+    private final CalculationModification<T, U> MODIFICATION;
+    private final T[] VALUES;
+
+    public AggregatorIterator(String name, CalculationModification<T, U> modification, T[] values){
+        NAME = name;
+        MODIFICATION = modification;
+        VALUES = values;
+    }
+
+    public String getName() {
+        return NAME;
+    }
+
+    public CalculationModification<T, U> getModification() {
+        return MODIFICATION;
+    }
+
+    @Override
+    public Iterator<U> iterator() {
+        return new ValuesIterator();
+    }
+
+    private class ValuesIterator implements Iterator<U>{
+
+        private U result = null;
+        private int nextId = 0;
+
+        @Override
+        public boolean hasNext() {
+            return nextId < VALUES.length;
+        }
+
+        @Override
+        public U next() {
+            result = (result != null)? MODIFICATION.applyModification(result, VALUES[nextId]):
+                                       MODIFICATION.convert(VALUES[nextId]);
+            nextId++;
+            return result;
+        }
+
+        public String nextString(){
+            return NAME + " iteration " + nextId + ": " + next();
+        }
+
+    }
+
+}
