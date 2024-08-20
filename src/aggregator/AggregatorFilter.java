5de78710299d0828221e39e39608855dd099165c
AP-JavaCod <megafoh3g@gmail.com>
2024-08-20 19:48:50 +0500

    Creation AggregatorFilter.java

---
+++ src/aggregator/AggregatorFilter.java
@@ -0,0 +1,42 @@
+package aggregator;
+
+import aggregator.lambda.Calculate;
+import aggregator.lambda.CalculationFilter;
+import aggregator.lambda.Conversion;
+import aggregator.lambda.Filter;
+
+public class AggregatorFilter <T, U> implements Aggregator<T, U>{
+
+    private final String NAME;
+    private final CalculationFilter<T, U> FILTER;
+
+    public AggregatorFilter(String name, CalculationFilter<T, U> filter){
+        NAME = name;
+        FILTER = filter;
+    }
+
+    public AggregatorFilter(String name, Calculate<U> calculate, Filter<T> filter, Conversion<T, U> conversion){
+        NAME = name;
+        FILTER = CalculationFilter.getInstance(calculate, filter, conversion);
+    }
+
+    public AggregatorFilter(String name, Calculate<U> calculate, Filter<T> filter){
+        NAME = name;
+        FILTER = (CalculationFilter<T, U>) CalculationFilter.getInstance((Calculate<T>) calculate, filter);
+    }
+
+    @Override
+    public U aggregation(T[] values) {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    public CalculationFilter<T, U> getFilter() {
+        return FILTER;
+    }
+
+}
