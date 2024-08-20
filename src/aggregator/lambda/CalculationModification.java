1d7db6af506c2b0fd733a821d4962b8bfc289683
AP-JavaCod <megafoh3g@gmail.com>
2024-08-20 17:59:28 +0500

    Creation CalculationModification.java

---
+++ src/aggregator/lambda/CalculationModification.java
@@ -0,0 +1,27 @@
+package aggregator.lambda;
+
+public abstract class CalculationModification <T, U> implements Calculate<U>, Conversion<T, U> {
+
+    public U applyModification(U val1, T val2){
+        return apply(val1, convert(val2));
+    }
+
+    public static <M, N> CalculationModification<M, N> getInstance(Calculate<N> calculate, Conversion<M, N> conversion){
+        return new CalculationModification<>() {
+            @Override
+            public N apply(N val1, N val2) {
+                return calculate.apply(val1, val2);
+            }
+
+            @Override
+            public N convert(M values) {
+                return conversion.convert(values);
+            }
+        };
+    }
+
+    public static <M> CalculationModification<M, M> getInstance(Calculate<M> calculate){
+        return getInstance(calculate, obj -> obj);
+    }
+
+}
