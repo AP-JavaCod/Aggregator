4cfeaa3b099c381a89b3ff5d2751f2bb50b96cae
AP-JavaCod <megafoh3g@gmail.com>
2024-08-20 18:11:50 +0500

    Creation AggregatorModification.java

---
+++ src/aggregator/AggregatorModification.java
@@ -0,0 +1,45 @@
+package aggregator;
+
+import aggregator.lambda.Calculate;
+import aggregator.lambda.CalculationModification;
+import aggregator.lambda.Conversion;
+
+public class AggregatorModification <T, U> implements  Aggregator<T, U> {
+
+    private final String NAME;
+    private final CalculationModification<T, U> MODIFICATION;
+
+    public AggregatorModification(String name, CalculationModification<T, U> modification){
+        NAME = name;
+        MODIFICATION = modification;
+    }
+
+    public AggregatorModification(String name, Calculate<U> calculate, Conversion<T, U> conversion){
+        NAME = name;
+        MODIFICATION = CalculationModification.getInstance(calculate, conversion);
+    }
+
+    public AggregatorModification(String name, Calculate<U> calculate){
+        NAME = name;
+        MODIFICATION = (CalculationModification<T, U>) CalculationModification.getInstance(calculate);
+    }
+
+    @Override
+    public U aggregation(T[] values) {
+        U result = MODIFICATION.convert(values[0]);
+        for (int i = 1; i < values.length; i++){
+            result = MODIFICATION.applyModification(result, values[i]);
+        }
+        return result;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    public CalculationModification<T, U> getMODIFICATION() {
+        return MODIFICATION;
+    }
+
+}
