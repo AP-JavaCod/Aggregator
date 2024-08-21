ce7aaaca8e0ebce5f9e8823d564d08d65d551ad5
AP-JavaCod <megafoh3g@gmail.com>
2024-08-21 10:04:20 +0500

    Test project

--- src/Test/TestProject.java
+++ src/Test/TestProject.java
@@ -1,5 +1,6 @@
 package Test;
 
+import aggregator.Aggregator;
 import aggregator.AggregatorFilter;
 import aggregator.AggregatorModification;
 import aggregator.lambda.CalculationModification;
@@ -9,34 +10,42 @@ import aggregator.modification.AggregatorIterator;
 public class TestProject {
 
     public static void main(String[] args) {
-        Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Integer[] array = {1, 4, 5, 8};
         String[] data = {"Java", "C++", "Java", "C#", "Kotlin", "JS", "Java", "C++", "JS"};
         CalculationModification<Integer, Integer> sumLambda = CalculationModification.getInstance(Integer::sum);
         CalculationModification<Integer, String> textLambda = CalculationModification.getInstance((s, t) -> s + t, i -> i + ", ");
         System.out.println("Aggregator modification");
         AggregatorModification<Integer, Integer> sumModification = sumLambda.getAggregator("Sum");
         AggregatorModification<Integer, String> textModification = textLambda.getAggregator("Text");
-        System.out.println(sumModification.aggregationString(array));
-        System.out.println(textModification.aggregationString(array));
+        print(sumModification);
+        print(textModification);
         System.out.println("Aggregator filter");
         AggregatorFilter<Integer, Integer> sumFilter = sumLambda.getFilter(i -> i % 2 == 0).getAggregator("Sum");
         AggregatorFilter<Integer, String> textFilter = textLambda.getFilter(i -> i % 2 == 0).getAggregator("Text");
-        System.out.println(sumFilter.aggregationString(array));
-        System.out.println(textFilter.aggregationString(array));
+        print(sumFilter);
+        print(textFilter);
         System.out.println("Aggregator filterX");
         AggregatorFilterX<Integer, Integer, String> sumFilterX = sumLambda.getAggregatorFilterX("Sum", s -> s.equals("Java"));
         AggregatorFilterX<Integer, String, String> textFilterX = textLambda.getAggregatorFilterX("Text", s -> s.equals("Java"));
-        System.out.println(sumFilterX.aggregation(array, data));
-        System.out.println(textFilterX.aggregation(array, data));
+        print(sumFilterX.getAggregator(data));
+        print(textFilterX.getAggregator(data));
         System.out.println("Aggregator Iterator");
         AggregatorIterator<Integer, Integer> sumIterator = sumLambda.getIterator("Sum", array);
         AggregatorIterator<Integer, String> textIterator = textLambda.getIterator("Text", array);
-        for (int i : sumIterator) {
+        print(sumIterator.getAggregator());
+        print(textIterator.getAggregator());
+        System.out.println("For each");
+        for (Integer i: sumIterator){
             System.out.println(i);
         }
-        for (String s : textIterator) {
-            System.out.println(s);
+        for (String t : textIterator){
+            System.out.println(t);
         }
+    }
+
+    public static void print(Aggregator<Integer, ?> aggregator){
+        Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(aggregator.aggregationString(array));
     }
 
 }
