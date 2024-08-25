c153b25d02227b1c1d79bbef8e8774cb2d5fe7dc
AP-JavaCod <megafoh3g@gmail.com>
2024-08-25 17:38:45 +0500

    Creation ContainerFunction.java

---
+++ src/aggregator/lambda/container/ContainerFunction.java
@@ -0,0 +1,36 @@
+package aggregator.lambda.container;
+
+import aggregator.lambda.Calculate;
+import aggregator.lambda.CalculationModification;
+import aggregator.lambda.Conversion;
+
+public abstract class ContainerFunction <T, U> implements Calculate<U>, Conversion<T, U> {
+
+    public U calculate(U result, T values){
+        return result != null ? apply(result, convert(values)) : convert(values);
+    }
+
+    public static <M, N> ContainerFunction<M, N> getInstance(Calculate<N> calculate, Conversion<M, N> conversion){
+        return new ContainerFunction<>() {
+            @Override
+            public N apply(N val1, N val2) {
+                return calculate.apply(val1, val2);
+            }
+
+            @Override
+            public N convert(M values) {
+                return conversion.convert(values);
+            }
+        };
+    }
+
+    public static <M> ContainerFunction<M, M> getInstance(Calculate<M> calculate){
+        return getInstance(calculate, obj -> obj);
+    }
+
+    @Deprecated
+    public static <M, N> ContainerFunction<M, N> getInstance(CalculationModification<M, N> modification){
+       return getInstance(modification, modification);
+    }
+
+}
