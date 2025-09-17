98610c9c284370c32fb30fbe5961bc64d9730b21
AP-JavaCode <megafoh3g@gmail.com>
2025-09-17 21:56:20 +0500

    AggregatorX extends AggregatorFunctional

--- src/com/apjc/aggregator/AggregatorX.java
+++ src/com/apjc/aggregator/AggregatorX.java
@@ -4,46 +4,32 @@ import com.apjc.aggregator.instructions.*;
 import com.apjc.aggregator.function.Calculator;
 import com.apjc.aggregator.function.Filter;
 
-public class AggregatorX <T, U> implements Aggregator<T, AggregatorX.Result<U>> {
-	
-	private final InstructionsContainer INSTRUCTIONS;
+public class AggregatorX <T, U> extends AggregatorFunctional<T, AggregatorX.Result<U>> {
 	
 	public AggregatorX(Instructions<? super T, U> ins1, Instructions<? super T, U> ins2) {
-		INSTRUCTIONS = new InstructionsContainer(ins1, ins2);
+		super(new InstructionsContainer<>(ins1, ins2));
 	}
 	
 	public AggregatorX(Instructions<? super T, U> ins1, Instructions<? super T, U> ins2, Filter<T> fil) {
-		INSTRUCTIONS = new InstructionsContainerFilter(ins1, ins2) {
+		super(new InstructionsContainerFilter<>(ins1, ins2) {
 			
 			@Override
 			public boolean filter(T values) {
 				return fil.filter(values);
 			}
 			
-		};
+		});
 	}
 	
 	public AggregatorX(InstructionsFilter<? super T, U> ins1, Instructions<? super T, U> ins2) {
-		INSTRUCTIONS = new InstructionsContainerFilter(ins1, ins2) {
+		super(new InstructionsContainerFilter<>(ins1, ins2) {
 			
 			@Override
 			public boolean filter(T values) {
 				return ins1.filter(values);
 			}
 			
-		};
-	}
-
-	@Override
-	public Result<U> aggregation(T[] values) {
-		Result<U> result = null;
-		for(T el : values) {
-			if(el == null) {
-				continue;
-			}
-			result = INSTRUCTIONS.applu(result, el);
-		}
-		return result;
+		});
 	}
 	
 	public U aggregation(T[] values, Calculator<U> cal) {
@@ -66,40 +52,40 @@ public class AggregatorX <T, U> implements Aggregator<T, AggregatorX.Result<U>> 
 		
 	}
 	
-	private class InstructionsContainer extends InstructionsFunctional<T, Result<U>>{
+	private static class InstructionsContainer<E, N> extends InstructionsFunctional<E, Result<N>>{
 		
-		private Instructions<? super T, U> instructions1;
-		private Instructions<? super T, U> instructions2;
+		private Instructions<? super E, N> instructions1;
+		private Instructions<? super E, N> instructions2;
 		
-		public InstructionsContainer(Instructions<? super T, U> ins1, Instructions<? super T, U> ins2) {
+		public InstructionsContainer(Instructions<? super E, N> ins1, Instructions<? super E, N> ins2) {
 			instructions1 = ins1;
 			instructions2 = ins2;
 		}
 
 		@Override
-		public AggregatorX.Result<U> calculate(AggregatorX.Result<U> val1, AggregatorX.Result<U> val2) {
-			U result1 = instructions1.calculate(val1.values1,  val2.values1);
-			U result2 = instructions2.calculate(val1.values2, val2.values2);
+		public AggregatorX.Result<N> calculate(AggregatorX.Result<N> val1, AggregatorX.Result<N> val2) {
+			N result1 = instructions1.calculate(val1.values1,  val2.values1);
+			N result2 = instructions2.calculate(val1.values2, val2.values2);
 			return new Result<>(result1, result2);
 		}
 
 		@Override
-		public AggregatorX.Result<U> transform(T values) {
-			U result1 = instructions1.transform(values);
-			U result2 = instructions2.transform(values);
+		public AggregatorX.Result<N> transform(E values) {
+			N result1 = instructions1.transform(values);
+			N result2 = instructions2.transform(values);
 			return new Result<>(result1, result2);
 		}
 		
 	}
 	
-	private abstract class InstructionsContainerFilter extends InstructionsContainer implements Filter<T>{
+	private abstract static class InstructionsContainerFilter<E, N> extends InstructionsContainer<E, N> implements Filter<E>{
 
-		public InstructionsContainerFilter(Instructions<? super T, U> ins1, Instructions<? super T, U> ins2) {
+		public InstructionsContainerFilter(Instructions<? super E, N> ins1, Instructions<? super E, N> ins2) {
 			super(ins1, ins2);
 		}
 
 		@Override
-		public Result<U> applu(Result<U> result, T values) {
+		public Result<N> applu(Result<N> result, E values) {
 			return filter(values) ? super.applu(result, values) : result;
 		}
 		
